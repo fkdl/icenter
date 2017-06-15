fa1dfb198d880f46f05ee22e3300494b0e0b847d
abler <abler@wln.io>
2017-06-15 20:46:43 +0800

    Version 170615

---
+++ wwwroot/vue/2.5.3/vue-xcenter.js
@@ -0,0 +1,69 @@
+﻿//浏览器兼容
+if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE6.0" || navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE7.0" || navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE8.0") {
+    alert("Your browser version is too low, please use IE9 and above or Chrome, Firefox and other browsers!");
+}
+var emptyPager = { size: 10, index: 1, total: 0, rows: [], key: '', message: '数据加载中' };
+Vue.prototype.getParam = (key) => {
+    var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    }
+    return "";
+}
+//设置cookie
+Vue.prototype.setCookie = (c_name, value, expiredays) => {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + expiredays);
+    document.cookie = c_name + "=" + escape(value) + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString());
+}
+//获取cookie
+Vue.prototype.getCookie = (name) => {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg)) {
+        return (arr[2]);
+    }
+    else {
+        return null;
+    }
+}
+//删除cookie
+Vue.prototype.delCookie = (name) => {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = Vue.prototype.getCookie(name);
+    if (cval != null) {
+        document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+    }
+}
+Vue.prototype.checkLogin = () => {
+    if (location.pathname == '/login' && Vue.prototype.getCookie('sid')) {
+        history.replaceState(null, '', '/');
+        location.reload();
+    } else if (location.pathname != '/login' && !Vue.prototype.getCookie('sid')) {
+        history.replaceState(null, '', '/login');
+        location.reload();
+    }
+}
+Vue.prototype.htmlEncode = (str) => {
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&/g, "&amp;");
+    s = s.replace(/</g, "&lt;");
+    s = s.replace(/>/g, "&gt;");
+    s = s.replace(/ /g, "&nbsp;");
+    s = s.replace(/\'/g, "&#39;");
+    s = s.replace(/\"/g, "&quot;");
+    return s;  
+}
+Vue.prototype.htmlDecode = (str) => {
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&amp;/g, "&");
+    s = s.replace(/&lt;/g, "<");
+    s = s.replace(/&gt;/g, ">");
+    s = s.replace(/&nbsp;/g, " ");
+    s = s.replace(/&#39;/g, "\'");
+    s = s.replace(/&quot;/g, "\"");
+    return s;
+}
